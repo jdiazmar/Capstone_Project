a6b0ad76b80edb440b019a768a29ddc67e7eb508
Josmar Diaz <jdm.begreat28@gmail.com>
2022-10-14 22:43:40 -0400

    Redirected Logos To Appropriate Social Media Links

--- frontend/src/components/SocialLogos/SocialLogos.jsx
+++ frontend/src/components/SocialLogos/SocialLogos.jsx
@@ -1,3 +1,5 @@
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import facebook from '../../images/FB-logo.jpg';
 import ig from '../../images/IG-logo.png';
 import tiktok from '../../images/Tiktok-logo.png';
@@ -5,10 +7,34 @@ import youtube from '../../images/Youtube-logo.png';
 
 function SocialLogos(){
     return <div>
-        <img className='navbar-brand' width='70' height='50' src={facebook} />
-        <img className='navbar-brand' width='70' height='50' src={ig} />
-        <img className='navbar-brand' width='70' height='50' src={tiktok} />
-        <img className='navbar-brand' width='70' height='50' src={youtube} />
+<Container>
+    <div>
+        <a href='https://www.facebook.com/LCYTH' >
+            <img className='navbar-brand' width='70' height='50' src={facebook} />
+        </a>
+        <a href='https://www.instagram.com/lakeviewyth/' >
+            <img className='navbar-brand' width='70' height='50' src={ig} />
+        </a>
+        <a href='https://www.tiktok.com/@lakeviewyth?lang=en' >
+            <img className='navbar-brand' width='70' height='50' src={tiktok} />
+        </a>
+        <a href='https://www.youtube.com/channel/UC6M355s3oLdiqiZBWGX6o7w' >
+            <img className='navbar-brand' width='70' height='50' src={youtube} />
+        </a>
+    </div>
+</Container>
     </div>
 }
 export default SocialLogos
+{/* <Link to='https://www.facebook.com/LCYTH' >
+<img className='navbar-brand' width='70' height='50' src={facebook} />
+</Link>
+<Link to='https://www.instagram.com/lakeviewyth/' >
+<img className='navbar-brand' width='70' height='50' src={ig} />
+</Link>
+<Link to='https://www.tiktok.com/@lakeviewyth?lang=en' >
+<img className='navbar-brand' width='70' height='50' src={tiktok} />
+</Link>
+<Link to='https://www.youtube.com/channel/UC6M355s3oLdiqiZBWGX6o7w' >
+<img className='navbar-brand' width='70' height='50' src={youtube} />
+</Link> */}
