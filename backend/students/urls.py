047020685a3c7de4979fcd956a002cae9070b7ef
Josmar Diaz <jdm.begreat28@gmail.com>
2022-10-17 19:44:34 -0400

    Modified URLs.py + Updated LeadersPage with Contact Info

--- backend/students/urls.py
+++ backend/students/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.student_list),
-    path('all/', views.student_detail),
+    path('<int:pk>/', views.student_detail),
 ]
--- frontend/src/pages/LeadersPage/LeadersPage.js
+++ frontend/src/pages/LeadersPage/LeadersPage.js
@@ -6,11 +6,16 @@ const LeadersPage = () => {
     return ( 
         <div className='container' >
             <div class='row' >
+                <h3>Meet the Youth Pastors!</h3>
                 <div class='col' >
                     <QImage />
+                    <p>Email:pastorq@lakeviewindy.com</p>
+                    <p>Phone:317-243-9396</p>
                 </div>
                 <div class='col' >
                     <LeaderImage />
+                    <p>Email:mattiew@lakeviewindy.com</p>
+                    <p>Phone:317-243-9396</p>
                 </div>
             </div>
         </div>
